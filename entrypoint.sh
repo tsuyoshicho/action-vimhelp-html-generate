ea049cb12a5e1b240a76c70bdab12b0d1f27d67b
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-11-02 17:39:15 +0900

    fix shebang

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-# based on https://github.com/w3ctag/promises-guide
-set -e # Exit with nonzero exit code if anything fails
-# based on https://qiita.com/youcune/items/fcfb4ad3d7c1edf9dc96
-set -u # Undefined variable use error
+#!/bin/sh
 
 # File Deploy
 make html
