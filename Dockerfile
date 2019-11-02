60e885748d6b071cbda3217bd372b902821ff641
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-11-02 17:46:44 +0900

    direct process not use make/Makefile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM alpine:3.10
 
-RUN apk --update add bash vim && \
+RUN apk --update add vim && \
     rm -rf /var/lib/apt/lists/* && \
     rm /var/cache/apk/*
 RUN mkdir /lib64 && \
     ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 
-COPY Makefile /Makefile
 COPY tools/ /tools
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+VIM = vim
+TARGET = $(FOLDER:-build)
+
 # File Deploy
-make html
+rm -rf $(TARGET)/
+mkdir -p $(TARGET)/generate
+cp doc/* $(TARGET)/generate
+cd $(TARGET)/generate; $(VIM) -eu ../../tools/buildhtml.vim -c "qall!"; cd -
+cp $(TARGET)/generate/*.html $(TARGET)/
+rm -rf $(TARGET)/generate
+cd $(TARGET);sh ../tools/genindex.sh > index.html; cd -
 
 # EOF
