6995e720d8b10cacd31a3888884194909bb7e5f9
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-11-06 15:28:55 +0900

    fix rm at find

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 rm -rf ${FOLDER}/
 cp -r doc ${FOLDER}
 cd ${FOLDER}/; vim -eu /tools/buildhtml.vim -c "qall!"; cd -
-find ${FOLDER}/ \( -name "*.txt" -or -name "*.??x" \) -type f -print0 | rm -f {} \;
+find ${FOLDER}/ \( -name "*.txt" -or -name "*.??x" \) -type f -exec rm -f {} +
 cd ${FOLDER};sh /tools/genindex.sh > index.html; cd -
 
 # EOF
