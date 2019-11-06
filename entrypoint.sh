8cd479f43f1de3cf766f3821e087eab09539d1b5
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-11-06 15:45:40 +0900

    rm comment out

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ fi
 rm -rf ${FOLDER}/
 cp -r doc ${FOLDER}
 cd ${FOLDER}/; vim -eu /tools/buildhtml.vim -c "qall!"; cd -
-find ${FOLDER}/ \( -name "*.txt" -or -name "*.??x" \) -type f -exec rm -f {} +
+# all delete, currently commented out.
+# find ${FOLDER}/ \( -name "*.txt" -or -name "*.??x" \) -type f -exec rm -f {} +
 cd ${FOLDER};sh /tools/genindex.sh > index.html; cd -
 
 # EOF
