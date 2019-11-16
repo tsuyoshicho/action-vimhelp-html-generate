c47582043db01b6fe8f493df5b6a8f4c4d3ed327
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-11-16 16:15:07 +0900

    fix pattern

--- tools/genindex.sh
+++ tools/genindex.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tree -H . -T "Auto Generate index : Vim Help(converted to HTML)" --charset=UTF-8 --noreport -P "*.html" -I "index.html|tags.*html" --prune
+tree -H . -T "Auto Generate index : Vim Help(converted to HTML)" --charset=UTF-8 --noreport -P "*.html" -I "index\.html|tags\.*html" --prune
